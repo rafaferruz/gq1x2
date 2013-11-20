3a5e2ae1282b47f2fa0295e2e942b7c8e4c7ff0a
rafaferruz@hotmail.com <rafaferruz@hotmail.com@6c7ac431-a1df-f8d5-7d1c-64e1b9157042>
2013-11-20 21:45:37 +0000

    Remove del directorio build

--- trunk/build/web/WEB-INF/Index.jsp
+++
@@ -1,9 +0,0 @@
-<%-- 
-    Document   : Index
-    Created on : 18-jun-2009, 19:16:47
-    Author     : ESTHER
---%>
-
-
-<jsp:forward page="/faces/index.xhtml"/>
-
--- trunk/build/web/WEB-INF/sqls/ppp.sql
+++
@@ -1,8 +0,0 @@
-select res_equ1_id, res_equ2_id,res_resul1,res_resul2,cla_equ_id,cla_gfc,cla_gcc,
-cla_gff,cla_gcf
-from resultados, campeonatos, clasificaciones
-where res_fecha>="2001-08-01" and res_ronda>4
-and (cto_id=res_cto_id and substr(cto_codigo,9,1)="1")
-and (cla_cto_id=res_cto_id and cla_fecha=res_fecha and 
-(cla_equ_id=res_equ1_id or cla_equ_id=res_equ2_id))
-order by res_fecha, res_id
