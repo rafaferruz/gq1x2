988bd6a7c02ac41411b2955b3d7b7e535eb6a2b3
rafaferruz@hotmail.com <rafaferruz@hotmail.com@6c7ac431-a1df-f8d5-7d1c-64e1b9157042>
2013-11-20 21:45:37 +0000

    Remove del directorio build

--- build/web/WEB-INF/sqls/extraer_rating4.sql
+++
@@ -1,8 +0,0 @@
-select rat4_cha_id, convert(rat4_previous_diference/10,SIGNED), rat4_score_sign,
-sum(if(rat4_score_sign='W',1,0)) as W, 
-sum(if(rat4_score_sign='D',1,0)) as D, 
-sum(if(rat4_score_sign='L',1,0)) as L,
- rat4_date
-from ratings4 as r
-group by rat4_cha_id
-order by rat4_date, rat4_cha_id
--- build/web/WEB-INF/sqls/resultados_futbol.sql
+++
@@ -1,5 +0,0 @@
-mysql> select cto.cto_codigo, res.res_ronda, equ1.equ_nombre, equ2.equ_nombre, r
-es.res_resul1, res.res_resul2 from resultados as res, campeonatos as cto, equipo
-s as equ1, equipos as equ2 where cto.cto_id = res.res_cto_id and equ1.equ_id = r
-es.res_equ1_id  and equ2.equ_id = res.res_equ2_id limit 3 INTO OUTFILE 'resultad
-os_futbol.txt';
