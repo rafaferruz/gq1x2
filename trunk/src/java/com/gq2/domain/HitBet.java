473e0d2465cba1c682a37055191849fc6668377d
rafaferruz@hotmail.com <rafaferruz@hotmail.com@6c7ac431-a1df-f8d5-7d1c-64e1b9157042>
2013-12-21 23:15:52 +0000

    Incluida tabla para visualizar los aciertos de una apuesta y los numeros de columnas para cada grupo de aciertos

---
+++ trunk/src/java/com/gq2/domain/HitBet.java
@@ -0,0 +1,42 @@
+package com.gq2.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.model.SelectItem;
+
+/**
+ *
+ * @author RAFAEL FERRUZ
+ */
+    public class HitBet {
+
+	private int hits;
+	private List<SelectItem> hitItemList = new ArrayList<>();
+
+	public HitBet(int hits, Integer numberColumn) {
+	    this.hits = hits;
+	    hitItemList.add(new SelectItem(numberColumn, numberColumn.toString()));
+	}
+
+	public int getHits() {
+	    return hits;
+	}
+
+	public void setHits(int hits) {
+	    this.hits = hits;
+	}
+
+	public List<SelectItem> getHitItemList() {
+	    return hitItemList;
+	}
+
+	public void setHitItemList(List<SelectItem> hitItemList) {
+	    this.hitItemList = hitItemList;
+	}
+
+	public HitBet addHit(Integer numberColumn) {
+	    setHits(getHits() + 1);
+	    hitItemList.add(new SelectItem(numberColumn, numberColumn.toString()));
+	    return this;
+	}
+    }
